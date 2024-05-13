607d62a02a7480a35460e37a5d0f16ef587070f2
cursedsound <cursedsoundproductions@gmail.com>
2024-05-13 18:02:25 +0200

    Added documentation to first unit test

--- app/backend/test_app.py
+++ app/backend/test_app.py
@@ -1,11 +1,14 @@
 import unittest
 # Use relative import for the app code
 from app import app
+# The following is the test case for the app.py file
 class TestApp(unittest.TestCase):
+    # Set up the test client
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-
+        
+    # This test checks if the connection to the /test_connection endpoint is successful
     def test_test_connection(self):
         response = self.app.get('/test_connection')
         self.assertEqual(response.status_code, 200)
@@ -13,4 +16,3 @@ class TestApp(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
