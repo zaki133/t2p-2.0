41265d2c43830083b4000a35155d6d46f63da63b
cursedsound <cursedsoundproductions@gmail.com>
2024-06-24 11:47:13 +0200

    Update test_xml_parser.py

--- app/unittest/test_xml_parser.py
+++ app/unittest/test_xml_parser.py
@@ -7,6 +7,9 @@ from backend.xml_parser import json_to_bpmn
 
 class TestJsonToBpmn(unittest.TestCase):
     def setUp(self):
+        """
+        Set up the bpmn_data.
+        """
         self.bpmn_data = {
             'events': [{'type': 'Start', 'id': 'start_event', 'name': 'Start Event'}],
             'tasks': [{'type': 'Task', 'id': 'task_1', 'name': 'Task 1'}],
@@ -16,8 +19,12 @@ class TestJsonToBpmn(unittest.TestCase):
 
     @patch("xml.etree.ElementTree.ElementTree.write")
     def test_json_to_bpmn(self, mock_write):
+        """
+        Test the json_to_bpmn function. It should be called twice.
+        """
         json_to_bpmn(self.bpmn_data)
-        mock_write.assert_called_once_with('bpmn_output.bpmn', encoding='utf-8', xml_declaration=True)
+        mock_write.assert_any_call('bpmn_output.bpmn', encoding='utf-8', xml_declaration=True)
+        self.assertEqual(mock_write.call_count, 2)
 
 if __name__ == '__main__':
     unittest.main()
